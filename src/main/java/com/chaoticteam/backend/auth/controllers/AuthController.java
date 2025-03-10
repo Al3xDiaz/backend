aa5208447d4e2f45c6a01e55904f6dd6ed5f9eff
al3xdiaz <alexleonel96@hotmail.com>
2025-03-09 18:59:11 -0600

    refactor: remove unused imports in AuthController

--- src/main/java/com/chaoticteam/backend/auth/controllers/AuthController.java
+++ src/main/java/com/chaoticteam/backend/auth/controllers/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,6 @@ import com.chaoticteam.backend.auth.dto.AuthenticationResponse;
 import com.chaoticteam.backend.auth.services.JwtService;
 import com.chaoticteam.backend.auth.services.UserDetailsServiceImp;
 import com.chaoticteam.backend.utils.HandleTransactionException;
-import com.nimbusds.jwt.JWT;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
